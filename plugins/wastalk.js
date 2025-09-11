9337f4715b52dd563b979dfaee27873fba33e5cb
Sachithra madusanka <Bloodxmd6@gmail.com>
2025-09-11 08:53:13 +0530

    Update wastalk.js

--- plugins/wastalk.js
+++ plugins/wastalk.js
@@ -38,8 +38,8 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363302677217436@newsletter',
-                    newsletterName: 'CASEYRHODES TECH 👻',
+                    newsletterJid: '120363419102725912@newsletter',
+                    newsletterName: 'BLOOD-XMD',
                     serverMessageId: 143
                 }
             }
